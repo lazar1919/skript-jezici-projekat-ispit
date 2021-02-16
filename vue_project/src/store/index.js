e258946e06c682fc11554d2b9c8c06b71e985411
Lazar Miladinovic <lmiladinovic2318rn@raf.rs>
2021-02-16 01:41:35 +0100

    zapocet vue

--- vue_project/src/store/index.js
+++ vue_project/src/store/index.js
@@ -5,10 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: []
   },
   mutations: {
   },
   actions: {
+    new_user: function({ commit }, user) {
+      fetch('http://localhost:8000/api/users/register', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: user
+      }).then((response) => {
+        if (!response.ok) {
+          console.log(response)
+          throw response;
+        }
+        return 'success'
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert(errorMessage);
+          });
+        else
+          alert(error);
+      });
+    },
+    login_user: function({ commit }, user){
+      fetch('http://localhost:8762/rest-airport-user-service/login', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: user
+      }).then((response) => {
+        if (!response.ok)
+          throw response;
+
+        this.state.token = response.headers.get("Authorization");
+        console.log(this.state.token);
+      }).catch((error) => {
+        if (typeof error.text === 'function')
+          error.text().then((errorMessage) => {
+            alert("Bad Credentials");
+          });
+        else
+          alert(error);
+      });
+    },
   },
   modules: {
   }
---
+++ vue_project/src/views/Register.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="register">
+        <b-container fluid>
+            <b-form>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-input v-model="newFirstName" class="mb-2 mr-sm-2 mb-sm-0" placeholder="First Name"></b-input>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-input v-model="newLastName" class="mb-2 mr-sm-2 mb-sm-0" placeholder="Last Name"></b-input>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-input v-model="newUsername" class="mb-2 mr-sm-2 mb-sm-0" placeholder="Username"></b-input>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-input v-model="newPassword" class="mb-2 mr-sm-2 mb-sm-0" placeholder="Password"></b-input>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-input v-model="newEmail" class="mb-2 mr-sm-2 mb-sm-0" placeholder="Email"></b-input>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-2">
+                    <b-col sm="3" offset="2">
+                        <b-button variant="primary" size="lg" @click="addNew">SignUp</b-button>
+                    </b-col>
+                </b-row>
+            </b-form>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import { mapActions, mapState } from 'vuex';
+
+    export default {
+        name: 'Register',
+        computed: {
+            
+        },
+        props: {
+            firstName: {
+                type: String,
+                default: ''
+            },
+            lastName: {
+                type: String,
+                default: ''
+            },
+            username: {
+                type: String,
+                default: ''
+            },
+            password: {
+                type: String,
+                default: ''
+            },
+            email: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                newFirstName: '',
+                newLastName: '',
+                newUsername: '',
+                newPassword: '',
+                newEmail: ''
+            }
+        },
+        mounted: function () {
+            this.newFirstName = this.firstName;
+            this.newLastName = this.lastName;
+            this.newUsername = this.username;
+            this.newPassword = this.password;
+            this.newEmail = this.email;
+        },
+        methods: {
+            ...mapActions(['new_user']),
+
+            addNew: function() {
+                const user = JSON.stringify({firstName: this.newFirstName, lastName: this.newLastName,
+                                                    username: this.newUsername, password: this.newPassword,
+                                                    email: this.email});
+
+                this.new_user(user);
+
+                this.newFirstName = '';
+                this.newLastName = '';
+                this.newUsername = '';
+                this.newPassword = '';
+                this.newEmail = '';
+            }
+        }
+    }
+</script>
